40633316104041d983ef15396868945fd0325023
Dale Bingham <dale.bingham@soteriasoft.com>
2025-12-02 07:02:04 -0500

    updated filter on templates

--- src/Controllers/TemplateController.cs
+++ src/Controllers/TemplateController.cs
@@ -384,17 +384,18 @@ namespace openrmf_templates_api.Controllers
                                 // remove the NIWC Enhanced type of ending
                                 stigType = stigType.Substring(0, stigType.IndexOf("- NIWC")).Trim();
                             }
-                            stigType = stigType.Replace("STIG", "Security Technical Implementation Guide").Replace("MS Windows","Windows")
-                                .Replace("SCAP Benchmark","").Replace(" SCAP","").Replace("Cisco IOS-XE","Cisco IOS XE").Replace("Cisco NX-OS", "Cisco NX OS")
-                                .Replace("Cisco IOS-XR","Cisco IOS XR")
-                                .Replace("Microsoft Windows","Windows").Replace("Dot Net","DotNet").Replace("Microsoft Windows Defender", "Microsoft Defender")
+                            stigType = stigType.Replace("STIG", "Security Technical Implementation Guide").Replace("MS Windows", "Windows")
+                                .Replace("SCAP Benchmark", "").Replace(" SCAP", "").Replace("Cisco IOS-XE", "Cisco IOS XE").Replace("Cisco NX-OS", "Cisco NX OS")
+                                .Replace("Cisco IOS-XR", "Cisco IOS XR")
+                                .Replace("Microsoft Windows", "Windows").Replace("Dot Net", "DotNet").Replace("Microsoft Windows Defender", "Microsoft Defender")
                                 .Replace("Windows Defender", "Microsoft Defender").Replace("Windows Server 2012 MS", "Windows Server 2012/2012 R2 Member Server")
                                 .Replace("Windows Firewall with Advanced Security", "Windows Defender Firewall with Advanced Security")
                                 .Replace("Microsoft Windows Defender Firewall with Advanced Security", "Windows Defender Firewall with Advanced Security")
                                 .Replace("Microsoft Defender Firewall with Advanced Security", "Windows Defender Firewall with Advanced Security")
+                                .Replace("Windows Firewall", "Windows Defender Firewall with Advanced Security")
                                 .Replace("Mozilla Firefox for Windows", "Mozilla Firefox").Replace("Mozilla Firefox for Linux", "Mozilla Firefox")
-                                .Replace("Mozilla Firefox for Unix", "Mozilla Firefox").Replace("IIS 10.0 Web Server","IIS 10.0 Server")
-                                .Replace("IIS 10.0 Web Site","IIS 10.0 Site")
+                                .Replace("Mozilla Firefox for Unix", "Mozilla Firefox").Replace("IIS 10.0 Web Server", "IIS 10.0 Server")
+                                .Replace("IIS 10.0 Web Site", "IIS 10.0 Site").Replace("MS SQL Server", "Microsoft SQL Server")
                                 .Trim();
 
                             if (!string.IsNullOrWhiteSpace(stigType)) stigType = stigType.Trim();
